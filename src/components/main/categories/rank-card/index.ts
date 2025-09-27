e8a889a2daae4e3d8c3b7879afa2601ee9cf84c4
kreatinj <kjune1999@naver.com>
2025-09-28 02:45:02 +0900

    fix: rankcard image

--- src/components/main/categories/rank-card/index.ts
+++ src/components/main/categories/rank-card/index.ts
@@ -14,8 +14,8 @@ export default function RankCard({ item, type }: Prop) {
   return `
   <li class="rank-card">
     <a href="#">
-      <img src="assets/main/rank/${item.rank}.svg" alt="rank ${item.rank}">
-      <img src="assets/main/${type}/${item.rank}.png" alt="${item.title}">
+      <img class="rank" src="assets/main/rank/${item.rank}.svg" alt="rank ${item.rank}">
+      <img class="thumbnail" src="assets/main/${type}/${item.rank}.png" alt="${item.title}">
     </a>
   </li>
 `;
