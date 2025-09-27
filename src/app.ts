1d12733876a5469b92cdd343c01acf6243169085
kreatinj <kjune1999@naver.com>
2025-09-28 02:05:45 +0900

    feat: attach main

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import "./global.css";
 import Footer from "~/components/footer";
 import Header from "~/components/header";
+import Main from "~/components/main";
 import handleModal from "./events/handleModal";
 import handleSlider from "./events/handleSlider";
 
-const header = await Header();
+const [header, main] = await Promise.all([Header(), Main()]);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     ${header}
+    ${main}
     ${Footer()}
   </div>
 `;
