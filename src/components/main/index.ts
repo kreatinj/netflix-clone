f549ddbd45924cb40f97d7e448d25d3f71952142
kreatinj <kjune1999@naver.com>
2025-09-28 02:42:47 +0900

    feat: attach categories

--- src/components/main/index.ts
+++ src/components/main/index.ts
@@ -1,10 +1,13 @@
 import "./index.css";
+import Categories from "./categories";
 import Hero from "./hero";
 
 export default async function Main() {
+  const categories = await Categories();
   return `
 <main class="main">
   ${Hero()}
+  ${categories}
 </main>
 `.trim();
 }
