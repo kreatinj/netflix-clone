10e628db0ad55b4713f74ff35a5ce4c9b4934eaf
kreatinj <kjune1999@naver.com>
2025-09-28 02:27:39 +0900

    feat: RegularCard

---
+++ src/components/main/categories/regular-card/index.ts
@@ -0,0 +1,20 @@
+import "./index.css";
+
+type Item = {
+  id: number;
+  title: string;
+}
+
+type Prop = {
+  item: Item;
+};
+
+export default function RegularCard({ item }: Prop) {
+  return `
+<li class="regular-card">
+  <a href="#">
+    <img class="item" src="assets/main/new/${item.id}.png" alt="${item.title}">
+  </a>
+</li>
+`;
+}
