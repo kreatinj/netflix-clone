fd0df9a84a850d8ca15781823d619d67cf9fbc7f
kreatinj <kjune1999@naver.com>
2025-09-28 12:09:32 +0900

    fix for build

--- src/events/handleSlider.ts
+++ src/events/handleSlider.ts
@@ -1,8 +1,8 @@
 export default function handleSlider(slider: Element) {
   const list = slider.querySelector("ol")!;
   const items = Array.from(list.querySelectorAll("li"));
-  const leftButton = slider.querySelector("button.left")!;
-  const rightButton = slider.querySelector("button.right")!;
+  // const leftButton = slider.querySelector("button.left")!;
+  // const rightButton = slider.querySelector("button.right")!;
   const pageIndicator = slider.parentElement!.querySelector(".page-indicator")!;
 
   const itemsPerScreen = 5;
@@ -17,4 +17,5 @@ export default function handleSlider(slider: Element) {
       indicator.classList.toggle("active", index === currentPage);
     });
   }
+  updatePageIndicator();
 }
