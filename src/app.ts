db33970022eaa86498b7df5bc540d1d84b97fc4c
kreatinj <kjune1999@naver.com>
2025-09-30 23:37:28 +0900

    feat: revert import LoadingSpinner first

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
 import "./global.css";
+import Footer from "~/components/footer";
+import Header from "~/components/header";
 import LoadingSpinner from "~/components/loading-spinner";
+import Main from "~/components/main";
+import handleCarousel from "~/events/handleCarousel";
+import handleLike from "~/events/handleLike";
+import handleModal from "~/events/handleModal";
 
 const appElement = document.querySelector<HTMLDivElement>('div#app')!;
 
 appElement.innerHTML = LoadingSpinner();
-
-const [Footer, Header, Main, handleModal, handleCarousel, handleLike] = await Promise.all([
-  import("~/components/footer").then(module => module.default),
-  import("~/components/header").then(module => module.default),
-  import("~/components/main").then(module => module.default),
-  import("~/events/handleModal").then(module => module.default),
-  import("~/events/handleCarousel").then(module => module.default),
-  import("~/events/handleLike").then(module => module.default),
-]);
 
 const [header, main] = await Promise.all([Header(), Main()]);
 appElement.innerHTML = `
